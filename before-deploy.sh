2d09c3723ada049ee10351d03dadfc378d1d6487
kimhangil <86448972+kimhangil@users.noreply.github.com>
2023-02-06 16:49:43 +0900

    Update before-deploy.sh

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
-if [-d /home/ec2-user/build ]; then
+if [ -d /home/ec2-user/build ]; then
     sudo rm -rf /home/ec2-user/build/
     sudo mkdir /home/ec2-user/build/
 fi
 
-if [-d /usr/local/tomcat/webapps/ROOT]; then
+if [ -d /usr/local/tomcat/webapps/ROOT ]; then
+    sudo rm -rf /usr/local/tomcat/webapps/ROOT.war
     sudo rm -rf /usr/local/tomcat/webapps/ROOT
 fi
 
